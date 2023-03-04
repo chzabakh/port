e0240a5dc3240f49067d5c1cd8eaec95a15fdd64
Charaf eddine Zabakh <chzabakh@e2r2p3.1337.ma>
2023-03-05 00:56:37 +0100

    working on an optimized portfolio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MyNav from './components/nav';
 
 function App() {
   return (
     <div className="App">
+      <MyNav />
       
     </div>
   );
